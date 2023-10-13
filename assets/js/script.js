0e8ee31104e7b723c72660e21eba89432499ea8c
Dave Pollak <davepollak@gmail.com>
2023-10-13 14:47:50 -0400

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById('moreBtn').addEventListener('click', function() {
+    var textElement = document.getElementById('text');
+    var btnElement = document.getElementById('moreBtn');
+
+    if (textElement.classList.contains('hidden')) {
+        textElement.classList.remove('hidden');
+        btnElement.innerText = 'Read Less';
+    } else {
+        textElement.classList.add('hidden');
+        btnElement.innerText = 'Read More';
+    }
+});
