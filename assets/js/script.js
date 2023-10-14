a4256c7e300b54dbb2e1cb0bea27af2c5a1888db
Dave Pollak <davepollak@gmail.com>
2023-10-13 23:32:39 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
-document.getElementById('moreBtn').addEventListener('click', function() {
-    var textElement = document.getElementById('text');
-    var btnElement = document.getElementById('moreBtn');
+document.addEventListener('DOMContentLoaded', function() {
+    // Select elements
+    var textElements = document.querySelectorAll('.text-content');
+    var btnElements = document.querySelectorAll('.read-more-btn');
 
-    if (textElement.classList.contains('hidden')) {
-        textElement.classList.remove('hidden');
-        btnElement.innerText = 'Read Less';
-    } else {
-        textElement.classList.add('hidden');
-        btnElement.innerText = 'Read More';
-    }
+    // Add click event listener to each button
+    btnElements.forEach(function(btnElement, index) {
+        btnElement.addEventListener('click', function() {
+            var textElement = textElements[index]; // Corresponding text element
+            if (textElement.classList.contains('hidden')) {
+                textElement.classList.remove('hidden'); // Show text
+                btnElement.innerText = 'Read Less'; // Change button text
+            } else {
+                textElement.classList.add('hidden'); // Hide text
+                btnElement.innerText = 'Read More'; // Change button text
+            }
+        });
+    });
 });
